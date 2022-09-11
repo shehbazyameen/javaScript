ff3293fb5d4077b1ef82c65d87700a778de0fd95
shehbazyameen <shehbazyameen43@gmail.com>
2022-09-10 23:45:41 -0700

    Template strings

---
+++ Variables/10_templateStrings.js
@@ -0,0 +1,8 @@
+//Template literals (Template strings)
+
+
+let string1 = 28;
+let string2 = "shehbazmalik";
+let newString = `My Name is ${string2} + age is ${string1};`
+console.log(newString);
+document.write(typeof newString + newString);
