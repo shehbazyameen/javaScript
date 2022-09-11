7f4d46f8b23c9174ce97e6a44aa182222d7dbb54
shehbazyameen <shehbazyameen43@gmail.com>
2022-09-10 23:39:55 -0700

    09_strint Concatenation

--- Variables/08_dataTypesPrimitive.js
+++ Variables/08_dataTypesPrimitive.js
@@ -1,31 +1,40 @@
-// typeof operator 
+// typeof operator
 
 // data types (primitive data types)
 
-
 // string "harhit"
-// number 2, 4, 5.6 
-// booleans true/false 
+// number 2, 4, 5.6
+// booleans true/false
 // undefined
-// null 
+// null
 // BigInt
 // Symbol
 
-// let age = 22; 
+// let age = 22;
 // let firstName = "harshit";
 
 // // console.log(typeof age);
 
 // // 22 -> "22"
-// // convert number to string. 
+
+// // convert number to string.
 // age = age + "";
 // console.log(typeof(age)); "22"
 
-
-// // convert string to number. 
+// // convert string to number.
 
 // let myStr = +"34";
 // console.log(typeof myStr);
+
+// document.write(typeof myStr);
+
+// 3) string convert to number
+
+// let age = "18";
+// age = Number(age);
+// console.log(typeof age);
+
+//4) number convert to string
 
 // let age = "18";
 // age = Number(age);
---
+++ Variables/09_strintConcatenation.js
@@ -0,0 +1,15 @@
+//string concatenation
+
+// let string1 = "17";
+// let string2 = "12";
+// let newString = string1 + string2;
+// console.log(newString);
+// document.write(typeof newString + newString);
+
+//string to Number
+
+let string1 = +"17";
+let string2 = +"12";
+let newString = string1 + string2;
+console.log(newString);
+document.write(typeof newString + newString);
