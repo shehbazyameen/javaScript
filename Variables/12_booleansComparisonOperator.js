0c538ba0eb354c6daed42da91e7f6252a286a51c
shehbazyameen <shehbazyameen43@gmail.com>
2022-09-15 22:33:06 -0700

    comparison operator

---
+++ Variables/12_booleansComparisonOperator.js
@@ -0,0 +1,24 @@
+//  * booleans & comparison operator
+
+// booleans
+// true, false
+
+let num1 = 7;
+let num2 = "7";
+console.log(num1 < num2);
+
+1; // == vs ===
+console.log(num1 == num2);
+
+// check both value and type of number
+console.log(num1 === num2);
+
+2; //  *    != vs !==
+
+console.log(num1 != num2);
+console.log(num1 !== num2);
+
+///Note
+
+//string or no ka khial rakhna ha both
+// == value check karta ha string or number
