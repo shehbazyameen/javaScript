48bc42534e0d9b6c5bdd0df871e30f10ed8af01d
shehbazyameen <shehbazyameen43@gmail.com>
2022-09-17 23:00:58 -0700

    falsy and truthy values

--- Basic_Part_01/13_truthyFalsy.js
+++ Basic_Part_01/13_truthyFalsy.js
@@ -8,5 +8,16 @@
 // 4    undefined
 // 5    0
 
+// let firstName =false;
+// let firstName ='' '';
+// let firstName = null;
+let firstName = undefined;
+// let firstName = 0;
 
+if (firstName) {
+  console.log("truthy values");
+} else {
+  console.log("falsy values");
+}
 
+//falsy values ha ya sab
