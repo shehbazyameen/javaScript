fe71def06c9da0970b23e29805cc8b9de377e531
shehbazyameen <shehbazyameen43@gmail.com>
2022-09-18 00:05:40 -0700

    and Or operators

---
+++ Basic_Part_01/16_andOROperators.js
@@ -0,0 +1,21 @@
+// and OR operators
+
+let firstName = "shehbaz";
+let age = 18;
+
+// AND && (check both condition)
+
+if (firstName[0] === "s" && age === 18) {
+  console.log("you are right person");
+} else {
+  console.log("you are wrong person");
+}
+
+
+//   ||  OR ()
+
+if (firstName[0] === "s" || age === 16) {
+  console.log("you are right person");
+} else {
+  console.log("you are wrong person");
+}
