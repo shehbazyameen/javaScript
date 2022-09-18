a76bffdd950232a3bff284e1695a1f7674743f08
shehbazyameen <shehbazyameen43@gmail.com>
2022-09-17 22:52:16 -0700

    even and odd no

---
+++ Variables/13_truthyFalsy.js
@@ -0,0 +1,12 @@
+//truthy and falsy values
+
+//falsy  values
+
+// 1    false
+// 2    ""
+// 3    null
+// 4    undefined
+// 5    0
+
+
+
---
+++ Variables/14_ifelseCondition.js
@@ -0,0 +1,17 @@
+//if else condition
+
+let age = 17;
+
+if (age > 18) {
+  console.log("you can play now");
+} else {
+  console.log("you can not play now gameF");
+}
+//check reminder and check even no
+let num = 14;
+console.log(num % 7);
+if (num % 2 === 0) {
+  console.log("even NO");
+} else {
+  console.log("Odd NO");
+}
