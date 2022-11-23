aa3b2a6d327455aeadc28f4ef3677399643081c7
shehbazyameen <50536514+shehbazyameen@users.noreply.github.com>
2022-11-23 09:52:56 -0800

    while loop

---
+++ Basic_Part_01/20_whileLoop.js
@@ -0,0 +1,14 @@
+//while loop
+
+//print no 0 to 9
+
+let i = 0;
+
+while (i <= 3) {
+  console.log(i);
+  i++;
+}
+
+//jab tak while complete nahi hoga tak tak alert pr nahi jaya ga
+
+console.log("end value of", i);
