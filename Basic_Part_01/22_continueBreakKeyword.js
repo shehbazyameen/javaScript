137aae5cb357aff32c09d39603edeb72159bf340
shehbazyameen <shehbazyameen43@gmail.com>
2022-11-27 21:57:47 -0800

    break and continue

---
+++ Basic_Part_01/22_continueBreakKeyword.js
@@ -0,0 +1,18 @@
+// break keyword
+
+for (let i = 0; i <= 9; i++) {
+  if (i == 4) {
+    break;
+  }
+  //   console.log(i);
+}
+
+// continue keyword
+// 4 skip hojaya ga 
+
+for (let i = 0; i <= 9; i++) {
+  if (i == 4) {
+    continue;
+  }
+  console.log(i);
+}
