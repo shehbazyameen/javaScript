83621014d68169e2392fa8097db3123fcf2ff431
shehbazyameen <shehbazyameen43@gmail.com>
2022-11-27 22:38:50 -0800

    aaray

---
+++ Basic_Part_01/24_array.js
@@ -0,0 +1,22 @@
+//Arrays
+//array is a reference data type
+//order collection of items //indexing
+//store any type of data and array bhi hosakta ha
+//immutable(content cannot be changed)
+// array ko object bhi bol skty ha aaray may
+
+let fruits = ["apple", "mango", "grapes"];
+
+console.log(fruits);
+console.log(fruits[2]);
+
+//replace
+console.log(fruits);
+fruits[1] = "banana";
+console.log(fruits);
+
+// 
+console.log(typeof fruits);
+//kasy pata chaly ga k ya array ha ya nhai 
+//to ha is k liya ya method use kary ga
+console.log(Array.isArray(fruits));
