c78f7feff93a8bcc99b97c27fa46a692d0e0429e
shehbazyameen <shehbazyameen43@gmail.com>
2022-11-27 21:50:32 -0800

    for loop

---
+++ Basic_Part_01/21_forLoop.js
@@ -0,0 +1,23 @@
+//for loop
+//print 0 to 9
+for (let i = 0; i < 9; ++i) {
+  console.log(i);
+}
+
+//interview question
+
+let j = 0;
+for (; j < 9; j++) {
+  console.log(j);
+}
+
+//for loop example
+
+let total = 0;
+
+for (let i = 0; i <= 10; i++) {
+  total = total + i;
+  console.log(total);
+}
+
+console.log(total);
