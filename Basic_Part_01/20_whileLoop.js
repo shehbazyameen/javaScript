f0816e0dcfdd59538aa9b6ba82d4b52fd371e43b
shehbazyameen <shehbazyameen43@gmail.com>
2022-11-27 21:27:17 -0800

    do

---
+++ Basic_Part_01/20_whileLoop.js
@@ -0,0 +1,15 @@
+//while loop
+
+// 0  se 9
+
+let initializeValue = 0;
+while (initializeValue < 9) {
+  console.log(initializeValue);
+  initializeValue++;
+}
+
+let i = 0;
+for (i; i < 9; i++) {
+  const element = i;
+  console.log(element);
+}
