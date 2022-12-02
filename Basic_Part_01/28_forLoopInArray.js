e24a46b3b79fad65e10930b46e3d8de929cd8e94
shehbazyameen <50536514+shehbazyameen@users.noreply.github.com>
2022-12-02 15:52:26 -0800

    for loop in array

---
+++ Basic_Part_01/28_forLoopInArray.js
@@ -0,0 +1,19 @@
+//for loop in array
+
+// programme 1
+
+let fruits1 = ["apple", "mango", "grapes", "banana"];
+for (let i = 0; fruits1.length > i; i++) {
+  console.log(fruits1[i]);
+}
+
+// programme 2
+//push value in new array
+
+let newArray = [];
+let fruits = ["apple", "mango", "grapes", "banana"];
+for (let i = 0; fruits.length > i; i++) {
+  newArray.push(fruits[i]);
+}
+
+console.log(newArray);
