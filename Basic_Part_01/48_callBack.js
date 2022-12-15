f49ac121dcd12b16e1195c13084b5113f09e36e7
Sameer Asad <50536514+shehbazyameen@users.noreply.github.com>
2022-12-15 13:53:05 -0800

    /callBack functions

---
+++ Basic_Part_01/48_callBack.js
@@ -0,0 +1,12 @@
+//callBack functions
+
+function MyFnc1(name) {
+  console.log("call as a callBack Fnc");
+  console.log("name is", name);
+}
+function MyFnc2(callBack) {
+  console.log("hello there i am a fnc and i can ...");
+  callBack("shehbaz");
+}
+
+MyFnc2(MyFnc1);
