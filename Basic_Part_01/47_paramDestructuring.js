544411bcfd7d510473f4d579035db97ba03076d4
Sameer Asad <50536514+shehbazyameen@users.noreply.github.com>
2022-12-15 13:36:52 -0800

    param Destructuring

---
+++ Basic_Part_01/47_paramDestructuring.js
@@ -0,0 +1,19 @@
+// param Destructuring
+
+//use in mostly
+//object
+//react
+
+let myObject = {
+  firstName: "shehbaz",
+  lastName: "Malik",
+  age: 23,
+};
+
+function myFunction(obj) {
+  console.log(obj.firstName);
+  console.log(obj.lastName);
+  console.log(obj.age);
+}
+
+myFunction(myObject);
