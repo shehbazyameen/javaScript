0c4794813c715ecfcb00ddd9b5838343ff5d5759
Sameer Asad <50536514+shehbazyameen@users.noreply.github.com>
2022-12-15 08:56:24 -0800

    lexical environment

--- Basic_Part_01/43_lexicalScope.js
+++ Basic_Part_01/43_lexicalScope.js
@@ -1,15 +1,13 @@
 //lexical Scope
 //lexical environment
-
+//javaScript poray environment may check karti ha 
+const myValue = "Lexical my Value1";
 function App() {
   const myValue = "Lexical my Value1";
-
   function myInsideFnc() {
     // const myValue = "myValue 2";
-
     console.log("My Lexical Var" + myValue);
   }
-
   console.log(myValue);
   myInsideFnc();
 }
